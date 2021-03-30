54550a0e1a66af953c23cc3576c97c42b4a20744
Sukistack <72786720+Sukistack@users.noreply.github.com>
2021-03-30 18:22:08 +1100

    Adding main files

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Your JS file is loaded correctly!"); 
+
+
+$(".resume").on("click", function(){
+    $(".resume").css("height", "200px" "width", "200px");
+});
+
+
+  $('h3').hover(function(){
+    $('.callToAction').addClass('active');
+},function(){
+    $('.callToAction').removeClass('active');
+});
+
+
+
